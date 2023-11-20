14c9a1bd8f0632ef58e3b7380d5a8532dd5368ff
Divyanshu naudiyal <divyanshunaudiyal@Divyanshus-MacBook-Air.local>
2023-11-20 21:10:34 +0530

    updated import for Layout and Project in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home.tsx";
-import Layout from "./layout.tsx";
+import Layout from "./Layout.tsx";
 import Contact from "./pages/contact/Contact.tsx";
 import About from "./pages/about/About.tsx";
-import Project from "./pages/project/project.tsx";
+import Project from "./pages/project/Project.tsx";
 function App() {
   return (
     <Routes>
